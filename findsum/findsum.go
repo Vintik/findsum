62f3e7af26ef64a0b8d492f2c42974039c613013
Victor <vintik@gmail.com>
2014-09-13 17:35:50 -0700

    FindSum BruteForce as was implemented in the interview and benchmark tests

--- findsum/findsum.go
+++ findsum/findsum.go
@@ -9,8 +9,9 @@ var (
 	errNotFound = fmt.Errorf("Sum is not found")
 )
 
-func FindSum(a []int, sum int) (int, int, error) {
-	var s, l, r, offset int
+func FindSum(a []int64, sum int64) (int, int, error) {
+	var s, offset int64
+	var l, r int
 
 	if sum == 0 {
 		offset = 1
@@ -41,4 +42,32 @@ func FindSum(a []int, sum int) (int, int, error) {
 
 		}
 	}
+}
+
+func FindSumBruteForce(a []int64, sum int64) (int, int, error) {
+
+	for l, _ := range a {
+		r := l
+		s := int64(0)
+
+		for {
+			if r >= len(a) {
+				break
+			}
+
+			s = s + a[r]
+
+			if s > sum {
+				break
+			}
+
+			if s == sum {
+				return l, r + 1, nil
+			}
+
+			r++
+		}
+	}
+
+	return 0, 0, errNotFound
 }
--- findsum/findsum_test.go
+++ findsum/findsum_test.go
@@ -2,11 +2,12 @@ package findsum
 
 import (
 	"fmt"
+	"math/rand"
 	"testing"
 )
 
 func ExampleFindSum() {
-	a := []int{1, 2, 3, 0, 1}
+	a := []int64{1, 2, 3, 0, 1}
 
 	l, r, _ := FindSum(a, 4)
 	fmt.Printf("found sum:%v in a:%v : %v left:%v, right:%v\n",
@@ -21,63 +22,115 @@ func ExampleFindSum() {
 }
 
 func ExampleFindSumErrNotFound() {
-	a := []int{1, 2, 10, 0, 1}
+	a := []int64{1, 2, 10, 0, 1}
 
 	_, _, err := FindSum(a, 8)
 	fmt.Printf("sum:8 in a:%v : %v\n", a, err)
 	// Output: sum:8 in a:[1 2 10 0 1] : Sum is not found
 }
 
-type TestCollection []struct {
-	name        string
-	sum         int
-	left, right int
-	err         error
-	a           []int
+func TestHappyPath(t *testing.T) {
+	testHappyPath(t, FindSum)
 }
 
-func TestHappyPath(testing *testing.T) {
-	tests := TestCollection{
-		{"happy-0-0", 0, 0, 1, nil, []int{0}},
-		{"happy-0-left", 0, 0, 1, nil, []int{0, 1, 1, 0}},
-		{"happy-0-right", 0, 2, 3, nil, []int{1, 1, 0}},
-		{"happy-0-middle", 0, 2, 3, nil, []int{1, 1, 0, 2, 2}},
-		{"happy-basic", 1, 0, 1, nil, []int{1}},
-		{"happy-all", 6, 0, 6, nil, []int{1, 1, 1, 1, 1, 1}},
-		{"happy-inside", 8, 1, 5, nil, []int{1, 2, 2, 2, 2, 1}},
-		{"happy-left", 8, 0, 4, nil, []int{2, 2, 2, 2, 1}},
-		{"happy-right", 8, 1, 5, nil, []int{1, 2, 2, 2, 2}},
-		{"happy-right-only", 10, 4, 5, nil, []int{1, 2, 2, 2, 10}},
-		{"happy-zeros-in-between", 20, 1, 6, nil, []int{1, 10, 0, 0, 0, 10}},
+func TestHappyPathBruteForce(t *testing.T) {
+	testHappyPath(t, FindSumBruteForce)
+}
+
+func TestNotFound(t *testing.T) {
+	testNotFound(t, FindSumBruteForce)
+}
+
+func TestNotFoundBruteForce(t *testing.T) {
+	testNotFound(t, FindSumBruteForce)
+}
+
+var bench = struct {
+	sum int64
+	a   []int64
+}{}
+
+func init() {
+	bench.a = make([]int64, 256*1024)
+	for i, _ := range bench.a {
+		bench.a[i] = int64(i)
 	}
 
-	for _, t := range tests {
-		l, r, err := FindSum(t.a, t.sum)
+	l := rand.Intn(len(bench.a) - 16*1024)
+	r := l + 16*1024
+	for _, v := range bench.a[l:r] {
+		bench.sum = bench.sum + v
+	}
+}
 
-		if l != t.left || r != t.right || err != t.err {
+func BenchmarkFindSum(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_, _, err := FindSum(bench.a, bench.sum)
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
+func BenchmarkFindSumBruteForce(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_, _, err := FindSumBruteForce(bench.a, bench.sum)
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
+type TestCollection []struct {
+	name        string
+	sum         int64
+	left, right int
+	err         error
+	a           []int64
+}
+
+func testHappyPath(t *testing.T, fs func([]int64, int64) (int, int, error)) {
+	tests := TestCollection{
+		{"happy-0-0", 0, 0, 1, nil, []int64{0}},
+		{"happy-0-left", 0, 0, 1, nil, []int64{0, 1, 1, 0}},
+		{"happy-0-right", 0, 2, 3, nil, []int64{1, 1, 0}},
+		{"happy-0-middle", 0, 2, 3, nil, []int64{1, 1, 0, 2, 2}},
+		{"happy-basic", 1, 0, 1, nil, []int64{1}},
+		{"happy-all", 6, 0, 6, nil, []int64{1, 1, 1, 1, 1, 1}},
+		{"happy-inside", 8, 1, 5, nil, []int64{1, 2, 2, 2, 2, 1}},
+		{"happy-left", 8, 0, 4, nil, []int64{2, 2, 2, 2, 1}},
+		{"happy-right", 8, 1, 5, nil, []int64{1, 2, 2, 2, 2}},
+		{"happy-right-only", 10, 4, 5, nil, []int64{1, 2, 2, 2, 10}},
+		{"happy-zeros-in-between", 20, 1, 6, nil, []int64{1, 10, 0, 0, 0, 10}},
+	}
+
+	for _, test := range tests {
+		l, r, err := fs(test.a, test.sum)
+
+		if l != test.left || r != test.right || err != test.err {
 			fmt.Printf("%v: sum:%v, a:%v, expected l:%v, r:%v, err:'%v', found l:%v, r:%v, err:'%v'\n",
-				t.name, t.sum, t.a, t.left, t.right, t.err, l, r, err)
-			testing.Fail()
+				test.name, test.sum, test.a, test.left, test.right, test.err, l, r, err)
+			t.Fail()
 
 		}
 	}
 
 }
 
-func TestNotFound(testing *testing.T) {
+func testNotFound(t *testing.T, fs func([]int64, int64) (int, int, error)) {
 	tests := TestCollection{
-		{"notFound", 25, 0, 0, errNotFound, []int{1, 1, 2, 5, 10, 20}},
-		{"notFound-empty", 0, 0, 0, errNotFound, []int{}},
-		{"notFound-zero", 0, 0, 0, errNotFound, []int{1}},
+		{"notFound", 25, 0, 0, errNotFound, []int64{1, 1, 2, 5, 10, 20}},
+		{"notFound-empty", 0, 0, 0, errNotFound, []int64{}},
+		{"notFound-zero", 0, 0, 0, errNotFound, []int64{1}},
 	}
 
-	for _, t := range tests {
-		l, r, err := FindSum(t.a, t.sum)
+	for _, test := range tests {
+		l, r, err := fs(test.a, test.sum)
 
-		if l != t.left || r != t.right || err != t.err {
+		if l != test.left || r != test.right || err != test.err {
 			fmt.Printf("%v: sum:%v, a:%v, expected l:%v, r:%v, err:'%v', found l:%v, r:%v, err:'%v'\n",
-				t.name, t.sum, t.a, t.left, t.right, t.err, l, r, err)
-			testing.Fail()
+				test.name, test.sum, test.a, test.left, test.right, test.err, l, r, err)
+			t.Fail()
 
 		}
 	}
